dca755ef4468426db4b8818a293a19f2ec4aff3b
kimdonggu42 <kdonggoo44@gmail.com>
2023-01-30 00:42:42 +0900

    todoInput, todoListContainer css 수정 및 중간 완성

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -62,42 +62,39 @@ function TodoList({ list, handleCheckChange, checkedItems, todoData, setTodoData
                 />
                 <label htmlFor={list.id}></label>
             </div>
-
             <div className="contentWrapper">
                 {/* todoContent : edit 버튼을 클릭하면 todoContent 수정 상태가 되고, 다시 edit 버튼을 클릭하면 수정한 todoContent가 저장된다. */}
                 {edit ?
                     <input className="editTodoContent" type='text' value={newContent} ref={editInputRef} onChange={onChangeEditInput}></input>
-                    // (<input className="editTodoContent" type='text' value={newContent} onChange={onChangeEditInput}></input>)
                     : <div className="todocontent">{list.content}</div>}
                 {/* 데이터 등록 날짜 */}
                 <div className="inputDate">{new Date(list.createdAt).toLocaleDateString()}</ div>
             </div>
-            <div className="buttonWrapper">
-                {/* todo 수정 버튼 : 평소(수정 상태가 아닐 때)에는 수정 버튼을 띄우고, 클릭 시 수정 완료 버튼을 띄운다. &&  체크된 todo라면 수정 버튼을 숨긴다. */}
-                {!checkedItems.includes(list.id) ?
-                    (edit ?
-                        <div className="complete">
-                            <button className="completeButton" onClick={onClickSubmitButton}><i className="fa-regular fa-square-check"></i></button>
+            {/* todo 수정 버튼 : 평소(수정 상태가 아닐 때)에는 수정 버튼을 띄우고, 클릭 시 수정 완료 버튼을 띄운다. &&  체크된 todo라면 수정 버튼을 숨긴다. */}
+            {!checkedItems.includes(list.id) ?
+                (edit ?
+                    <div className="complete">
+                        <button className="completeButton" onClick={onClickSubmitButton}><i className="fa-regular fa-square-check"></i></button>
+                    </div>
+                    : <div className="edit">
+                        <button className="editButton" onClick={onClickEditButton}><i className="fa-regular fa-pen-to-square"></i></button>
+                    </div>)
+                : null}
+            {/* todo 삭제 모달 팝업 버튼 */}
+            <div className="delete">
+                <button className="popUpDeleteModal" onClick={openModalHandler}><i className="fa-regular fa-trash-can"></i></button>
+                {isOpen ?
+                    <div className="modalBackdrop">
+                        <div className="modalView" onClick={stopEvent}>
+                            <img className="warningIcon" src="../img/warning.png" alt="warning logo" />
+                            <div className="warningMessage">정말 삭제하시겠습니까?</div>
+                            <button className="cancelButton" onClick={openModalHandler}>취소</button>
+                            <button className="deleteButton" onClick={() => deleteButton(list.id)}>삭제</button>
                         </div>
-                        : <div className="edit">
-                            <button className="editButton" onClick={onClickEditButton}><i className="fa-regular fa-pen-to-square"></i></button>
-                        </div>)
+                    </div>
                     : null}
-                {/* todo 삭제 모달 팝업 버튼 */}
-                <div className="delete">
-                    <button className="popUpDeleteModal" onClick={openModalHandler}><i className="fa-regular fa-trash-can"></i></button>
-                    {isOpen ?
-                        <div className="modalBackdrop">
-                            <div className="modalView" onClick={stopEvent}>
-                                <img className="warningIcon" src="../img/warning.png" alt="warning logo" />
-                                <div className="warningMessage">정말 삭제하시겠습니까?</div>
-                                <button className="cancelButton" onClick={openModalHandler}>취소</button>
-                                <button className="deleteButton" onClick={() => deleteButton(list.id)}>삭제</button>
-                            </div>
-                        </div>
-                        : null}
-                </div>
             </div>
+
         </li>
     );
 }
